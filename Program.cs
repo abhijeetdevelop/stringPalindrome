6205ed19398ef73fa5498ad9ea3936d081ac1468
Abhijeet <abhijeet.abhijeet@soprasteria.com>
2019-09-13 00:47:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice1309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "noon";
+            char[] ch = new char[str1.Length];
+            ch = str1.ToCharArray();
+            ch.Reverse();
+            string newStr = new string(ch);     
+            
+            if (newStr == str1)
+                Console.WriteLine("Palindrome");
+            else Console.WriteLine("Not a Palindrome");
+
+        }
+    }
+}
